e593e277bbeb03dbda81b510a22f961e45b3d10b
EnsekiTT <ensekitt@gmail.com>
2017-09-01 23:56:35 +0900

    えるーちかしました

---
+++ led_chika/main/led_chika_main.c
@@ -0,0 +1,23 @@
+/*
+かしこいかわいいえるーちか。
+*/
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_event.h"
+#include "esp_system.h"
+#include "driver/gpio.h"
+
+void app_main()
+{
+  printf("LED chika chika!\n");
+  tcpip_adapter_init();
+  gpio_set_direction(GPIO_NUM_4, GPIO_MODE_OUTPUT);
+  int level = 0;
+  while (true) {
+    gpio_set_level(GPIO_NUM_4, level);
+    printf("now %d\n", level);
+    level = !level;
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
+  }
+}
